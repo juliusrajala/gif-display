30ccc20879f6451c17e5f660b9b6395ea9d055af
Julius Rajala <juliusrajala@gmail.com>
2020-12-05 10:35:38 +0200

    Basic form to display images with.

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
-
+  import GifSelector from './components/GifSelector.svelte';
   export let name: string;
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div>
+    <h1>Hello {name}!</h1>
+    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  </div>
+  <GifSelector />
 </main>
 
 <style>
   main {
     text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 100%;
+  }
+
+  main > * {
+      flex: 1;
   }
 
   h1 {
@@ -23,9 +31,10 @@
     font-weight: 100;
   }
 
-  @media (min-width: 640px) {
+  @media (max-width: 640px) {
     main {
       max-width: none;
+      flex-direction: column;
     }
   }
 </style>
---
+++ src/components/GifSelector.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  export let gifUrl: string = 'https://media.giphy.com/media/2uksuufgQJ5VXJegxX/giphy.gif';
+</script>
+
+<section>
+  <div class="GifDisplay">
+    <p>{gifUrl}</p>
+    <img src={gifUrl} />
+  </div>
+  <div class="GifSelect">
+  <label>
+    <span>Link to your gif:</span>
+    <input bind:value={gifUrl} />
+  </label>
+  <button>Save me</button>
+  </div>
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    max-width: 50vw;
+    height: 100%;
+    width: 100%;
+    color: #fff;
+    background: #323232;
+    padding: 4rem;
+  }
+
+
+  .GifDisplay {
+    width: 100%;
+    margin-bottom: 4rem;
+  }
+
+  .GifDisplay > img {
+    width: 100%;
+    border-radius: 3px;
+  }
+
+  .GifSelect > label {
+    display: flex;
+    flex-direction: column;
+  }
+
+  input {
+    background: transparent;
+    border: none;
+    border-bottom: 1px solid #f9f9f9;
+    color: #f2f2f2;
+    text-align: center;
+  }
+
+</style>
